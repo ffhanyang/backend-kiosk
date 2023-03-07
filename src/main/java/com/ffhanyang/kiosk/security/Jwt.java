ffb38831d4dd5db85dee3a2dd8130d80f97348e1
GyeongHoKim <rlarudgh2017@gmail.com>
2023-03-07 20:33:23 +0900

    feat(jwt): add Jwt and Configure files

---
+++ src/main/java/com/ffhanyang/kiosk/security/Jwt.java
@@ -0,0 +1,143 @@
+package com.ffhanyang.kiosk.security;
+
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.ffhanyang.kiosk.model.member.Email;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public final class Jwt {
+
+    private final String issuer;
+
+    private final String clientSecret;
+
+    private final int expirySeconds;
+
+    private final Algorithm algorithm;
+
+    private final JWTVerifier jwtVerifier;
+
+    public Jwt(String issuer, String clientSecret, int expirySeconds) {
+        this.issuer = issuer;
+        this.clientSecret = clientSecret;
+        this.expirySeconds = expirySeconds;
+        this.algorithm = Algorithm.HMAC512(clientSecret);
+        this.jwtVerifier = com.auth0.jwt.JWT.require(algorithm)
+            .withIssuer(issuer)
+            .build();
+    }
+
+    public String newToken(Claims claims) {
+        Date now = new Date();
+        JWTCreator.Builder builder = com.auth0.jwt.JWT.create();
+        builder.withIssuer(issuer);
+        builder.withIssuedAt(now);
+        if (expirySeconds > 0) {
+            builder.withExpiresAt(new Date(now.getTime() + expirySeconds * 1_000L));
+        }
+        builder.withClaim("memberKey", claims.memberKey);
+        builder.withClaim("email", claims.email.getAddress());
+        builder.withArrayClaim("roles", claims.roles);
+        return builder.sign(algorithm);
+    }
+
+    public String refreshToken(String token) throws JWTVerificationException {
+        Claims claims = verify(token);
+        claims.eraseIat();
+        claims.eraseExp();
+        return newToken(claims);
+    }
+
+    public Claims verify(String token) throws JWTVerificationException {
+        return new Claims(jwtVerifier.verify(token));
+    }
+
+    public String getIssuer() {
+        return issuer;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public int getExpirySeconds() {
+        return expirySeconds;
+    }
+
+    public Algorithm getAlgorithm() {
+        return algorithm;
+    }
+
+    public JWTVerifier getJwtVerifier() {
+        return jwtVerifier;
+    }
+
+    static public class Claims {
+        Long memberKey;
+        Email email;
+        String[] roles;
+        Date iat;
+        Date exp;
+
+        private Claims() {
+        }
+
+        Claims(DecodedJWT decodedJWT) {
+            Claim memberKey = decodedJWT.getClaim("memberKey");
+            if (!memberKey.isNull())
+                this.memberKey = memberKey.asLong();
+            Claim email = decodedJWT.getClaim("email");
+            if (!email.isNull())
+                this.email = new Email(email.asString());
+            Claim roles = decodedJWT.getClaim("roles");
+            if (!roles.isNull())
+                this.roles = roles.asArray(String.class);
+            this.iat = decodedJWT.getIssuedAt();
+            this.exp = decodedJWT.getExpiresAt();
+        }
+
+        public static Claims of(long memberKey, Email email, String[] roles) {
+            Claims claims = new Claims();
+            claims.memberKey = memberKey;
+            claims.email = email;
+            claims.roles = roles;
+            return claims;
+        }
+
+        long iat() {
+            return iat != null ? iat.getTime() : -1;
+        }
+
+        long exp() {
+            return exp != null ? exp.getTime() : -1;
+        }
+
+        void eraseIat() {
+            iat = null;
+        }
+
+        void eraseExp() {
+            exp = null;
+        }
+
+        @Override
+        public String toString() {
+            return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("memberKey", memberKey)
+                .append("email", email)
+                .append("roles", Arrays.toString(roles))
+                .append("iat", iat)
+                .append("exp", exp)
+                .toString();
+        }
+    }
+
+}
