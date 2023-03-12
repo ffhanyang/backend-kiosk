9daf4325ed543ddf100a89f3a6937392ccb8c2d5
GyeongHoKim <rlarudgh2017@gmail.com>
2023-03-12 14:36:19 +0900

    feat(security): add WithMockJwtAuthentication annotation and its factory
    In test code, we can set authentication context by using
    @WithMockJwtAuthentication
    default set is already added when server starts.

---
+++ src/test/java/com/ffhanyang/kiosk/security/WithMockJwtAuthentication.java
@@ -0,0 +1,16 @@
+package com.ffhanyang.kiosk.security;
+
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithMockJwtAuthenticationSecurityContextFactory.class)
+public @interface WithMockJwtAuthentication {
+    long id() default 1L;
+
+    String email() default "tester0";
+
+    String role() default "ROLE_USER";
+}
